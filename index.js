7748c392fd15a13f7fb9c1aa0e1d1caeac9c2b74
Collin Arnold <collin@collinarnold.net>
2018-11-27 19:20:27 -0800

    setting up the watcher window

---
+++ index.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+const MenuBar = require('menubar')
+const mb = new MenuBar({
+    icon: 'icon.ico',
+    index: path.join(__dirname, 'index.html'),
+    tooltip: 'AWS EC2 Watcher'
+})
+
+mb.on('ready', () => {
+    console.log('ready')
+})
